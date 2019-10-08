b0be295664001b74d7b4ce389b3583efcac97cfb
Dianna A Toledo <56136290+DiannaToledoSG@users.noreply.github.com>
2019-10-08 14:48:52 -0700

    Add files via upload

---
+++ Hello_World/src/hello_world.java
@@ -0,0 +1,28 @@
+//HEADER 
+//Program Name: Chapter 1 Exercise 2 and 3
+//Author: Dianna A.H.H. Toledo
+//Class: CS160 Fall 2019
+//Date: 10/8/2019
+// Description: Create the "Hello World!"
+// program with an additional sentence. 
+// As well as test out different errors.
+
+public class hello_world {
+
+	public static void main(String[] args) {
+		// I love program comments. They are great
+		System.out.println("Hello,World!");
+		System.out.println("Hello. my name is Inigo Montoya. You killed my father. Prepare to die");
+	}
+
+}
+
+/* FOOTER
+ *  
+ **
+ ***
+ ****
+ *****
+ *Hello,World!
+Hello. my name is Inigo Montoya. You killed my father. Prepare to die
+ */
